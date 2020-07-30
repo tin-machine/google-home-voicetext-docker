9e012ff2f936dd2bbc2ac7316a9f1de9d249f6cf
kaoru inoue <kaoru-inoue@aoi-zemi.com>
2020-07-30 16:03:13 +0900

    デーモンが起動していない可能性があるので、起動時、少し待つことにする

--- boot.bash
+++ boot.bash
@@ -4,6 +4,7 @@ patch -u node_modules/mdns/lib/browser.js < mdns_patch/browser.js.patch
 
 service dbus start
 service avahi-daemon start
+sleep 10
 node file-server.js &
 node firestore.js &
 node api-server.js
