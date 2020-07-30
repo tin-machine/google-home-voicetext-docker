54d8bc4300a1183f3b2ea0b6601ecbfffcc766f9
kaoru inoue <kaoru-inoue@aoi-zemi.com>
2020-07-30 21:24:15 +0900

    avahiの前にdbusが起動している必要がありそうなのでsleepを入れる

--- boot.bash
+++ boot.bash
@@ -3,8 +3,9 @@
 patch -u node_modules/mdns/lib/browser.js < mdns_patch/browser.js.patch
 
 service dbus start
+sleep 3
 service avahi-daemon start
-sleep 10
+sleep 3
 node file-server.js &
 node firestore.js &
 node api-server.js
