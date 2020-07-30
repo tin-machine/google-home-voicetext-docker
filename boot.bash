dd6ef8d4ec07caa3afb61c3e70162ca829cb2685
kaoru inoue <kaoru-inoue@aoi-zemi.com>
2020-07-30 22:44:16 +0900

    avahi-daemonの起動方法を変更
    avahi-daemon -D <- これは --daemonize だが、これだと起動できないので
    avahi-daemon --daemonize --no-drop-root という形で起動するようにした。

--- boot.bash
+++ boot.bash
@@ -1,12 +1,9 @@
 #/bin/bash
 
-patch -u node_modules/mdns/lib/browser.js < mdns_patch/browser.js.patch
+# patch -u node_modules/mdns/lib/browser.js < mdns_patch/browser.js.patch
 
 service dbus start
-sleep 3
-service avahi-daemon start
-sleep 3
+avahi-daemon --daemonize --no-drop-root
 node file-server.js &
 node firestore.js &
-sleep 30
 node api-server.js
